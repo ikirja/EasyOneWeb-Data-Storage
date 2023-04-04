fdc51f4aa68a066721e3d64fa4ce693e840112f3
Kirill Makeev <kirill.makeev@icloud.com>
2023-04-04 12:26:39 +0300

    post create event new url

--- rest-api/v1/index.js
+++ rest-api/v1/index.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const { events } = require('./controllers');
 
 router.post('/events', events.getEvents);
-router.post('/events', events.createEvents);
+router.post('/events/create', events.createEvents);
 
 module.exports = router;
