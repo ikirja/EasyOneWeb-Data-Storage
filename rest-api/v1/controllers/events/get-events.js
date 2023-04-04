f25e039ae7c9dd20e13397dd9b64214759388e99
Kirill Makeev <kirill.makeev@icloud.com>
2023-04-04 12:48:27 +0300

    clearEvents in post events

--- rest-api/v1/controllers/events/get-events.js
+++ rest-api/v1/controllers/events/get-events.js
@@ -1,21 +1,24 @@
 const { DB } = require(`${__basedir}/lib`);
 
 async function getEvents(req, res) {
-  const DATE_START = req.body.dateStart;
-  const DATE_END = req.body.dateEnd;
+  const { DATE_START, DATE_END, clearEvents } = req.body.dateStart;
   const gte = new Date(DATE_START);
   const lt = new Date(DATE_END);
 
   if (isNaN(gte) || isNaN(lt)) return res.status(422).json({ error: true, message: 'Check dates' });
 
+  const queryObject = {
+    createdAt: {
+      $gte: gte,
+      $lt: lt
+    }
+  }
+
   try {
-    const events = await new DB().events.getEvents({
-      createdAt: {
-        $gte: gte,
-        $lt: lt
-      }
-    });
+    const events = await new DB().events.getEvents(queryObject);
     res.status(200).json(events);
+
+    if (clearEvents) await new DB().events.deleteEvents(queryObject);
   } catch (error) {
     // TODO: ERROR HANDLER
     res.status(400).json({ error: true, message: 'Get Events' });
