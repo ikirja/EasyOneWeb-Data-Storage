174558262574317db4a5e0c86f42c4655d48e743
Kirill Makeev <kirill.makeev@icloud.com>
2023-04-04 11:58:03 +0300

    getEvents by period

--- rest-api/v1/controllers/events/get-events.js
+++ rest-api/v1/controllers/events/get-events.js
@@ -1,8 +1,20 @@
 const { DB } = require(`${__basedir}/lib`);
 
 async function getEvents(req, res) {
+  const DATE_START = req.body.dateStart;
+  const DATE_END = req.body.dateEnd;
+  const gte = new Date(DATE_START);
+  const lt = new Date(DATE_END);
+
+  if (isNaN(gte) || isNaN(lt)) return res.status(422).json({ error: true, message: 'Check dates' });
+
   try {
-    const events = await new DB().events.getEvents();
+    const events = await new DB().events.getEvents({
+      createdAt: {
+        $gte: gte,
+        $lt: lt
+      }
+    });
     res.status(200).json(events);
   } catch (error) {
     // TODO: ERROR HANDLER
